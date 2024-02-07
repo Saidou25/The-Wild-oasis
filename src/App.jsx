0e2f05a792bfd1d1a67f004a9aa7328836752d4e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-02-07 08:56:29 -0500

    finished styling reusable Button component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import GlobalStyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -15,12 +16,22 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in an out</Heading>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in an out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <form>
+              <Heading as="h3">Form</Heading>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
