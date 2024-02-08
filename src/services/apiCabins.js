98841f558ae9c48207f041a90a7cdf9eac489e42
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-02-08 13:55:59 -0500

    worked on createCabinForm, working smoothly, error handling must be completed

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,8 +4,18 @@ export default async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.log(error);
     throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    throw new Error("Cabins could not be created");
   }
   return data;
 }
